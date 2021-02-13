90398b79808cdd6e9e2809d0c5f86aeb7520d68c
Nhoppasit <s.saejia@gmail.com>
2021-02-13 18:27:51 +0700

    Add pulse count

--- SRC/oled_ti_1kHz_gen_count_01/oled_ti_1kHz_gen_count_01.ino
+++ SRC/oled_ti_1kHz_gen_count_01/oled_ti_1kHz_gen_count_01.ino
@@ -5,21 +5,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306_STM32.h>
 
+/* Timer interrupt for pulse generation and counting */
 #define LED_RATE 500 // in microseconds; should give 1.0kHz toggles
 bool ticktok = false;
 bool OutputFlag = false;
+int OnCount = 0;
 
+/* Adc moving average */
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
-
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH 16
-
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
@@ -42,26 +38,32 @@ void setup()
   Serial.begin(9600);
 
   pinMode(Adc0Pin, INPUT_ANALOG);
+
   pinMode(PB12, INPUT);
   pinMode(PB13, INPUT);
+
   pinMode(PC13, OUTPUT);
   pinMode(PB1, OUTPUT);
   pinMode(PB8, OUTPUT);
+  pinMode(PB9, OUTPUT);
 
   for (int i = 0; i < 3; i++)
   {
-    digitalWrite(PC13, 1);
+    digitalWrite(PC13, 0);
     digitalWrite(PB1, 1);
     digitalWrite(PB8, 1);
+    digitalWrite(PB9, 1);
     delay(150);
-    digitalWrite(PC13, 0);
+    digitalWrite(PC13, 1);
     digitalWrite(PB1, 0);
     digitalWrite(PB8, 0);
+    digitalWrite(PB9, 0);
     delay(250);
   }
-  digitalWrite(PC13, 0);
+  digitalWrite(PC13, 1);
   digitalWrite(PB1, 0);
   digitalWrite(PB8, 0);
+  digitalWrite(PB8, 9);
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3D (for the 128x64)
@@ -78,14 +80,13 @@ void setup()
   display.display();
 
   // invert the display
-  display.invertDisplay(true);
-  delay(500);
-  display.invertDisplay(false);
-  delay(500);
-  display.invertDisplay(true);
-  delay(500);
-  display.invertDisplay(false);
-  delay(500);
+  for (int i = 0; i < 0; i++)
+  {
+    display.invertDisplay(true);
+    delay(500);
+    display.invertDisplay(false);
+    delay(500);
+  }
   display.clearDisplay();
   display.display();
 
@@ -130,7 +131,9 @@ void ReadAdc(bool flag)
       {
         AdcReadIndex = 0;
 #if TRACE_ADC
-        Serial.print("A0 = ");
+        Serial.print("Pulse Count = ");
+        Serial.print(OnCount);
+        Serial.print(" | A0 = ");
         Serial.print(Adc0Value);
         Serial.print(", Vref = ");
         Serial.print(VREF, 3);
@@ -150,48 +153,33 @@ void handler_led(void)
 {
   if (OutputFlag)
   {
+    digitalWrite(PC13, 0);
     if (ticktok)
     {
-      digitalWrite(PC13, 1);
       digitalWrite(PB1, 1);
       digitalWrite(PB8, 1);
+      digitalWrite(PB9, 1);
+      OnCount++;
     }
     else
     {
-      digitalWrite(PC13, 0);
       digitalWrite(PB1, 0);
       digitalWrite(PB8, 0);
+      digitalWrite(PB9, 0);
     }
     ticktok = !ticktok;
   }
-}
-
-void testdrawchar(void)
-{
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-
-  for (uint8_t i = 0; i < 168; i++)
+  else
   {
-    if (i == '\n')
-      continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
-}
-
-void testdrawcircle(void)
-{
-  for (int16_t i = 0; i < display.height(); i += 2)
-  {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, WHITE);
-    display.display();
+    OnCount = 0;
+    digitalWrite(PC13, 1);
+    digitalWrite(PB1, 0);
+    digitalWrite(PB8, 0);
+    digitalWrite(PB9, 0);
   }
 }
 
+///
 void testfillrect(void)
 {
   uint8_t color = 1;
@@ -202,141 +190,4 @@ void testfillrect(void)
     display.display();
     color++;
   }
-}
-
-void testdrawtriangle(void)
-{
-  for (int16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5)
-  {
-    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    display.display();
-  }
-}
-
-void testfilltriangle(void)
-{
-  uint8_t color = WHITE;
-  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5)
-  {
-    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    if (color == WHITE)
-      color = BLACK;
-    else
-      color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawroundrect(void)
-{
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2)
-  {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, WHITE);
-    display.display();
-  }
-}
-
-void testfillroundrect(void)
-{
-  uint8_t color = WHITE;
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2)
-  {
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
-    if (color == WHITE)
-      color = BLACK;
-    else
-      color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawrect(void)
-{
-  for (int16_t i = 0; i < display.height() / 2; i += 2)
-  {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, WHITE);
-    display.display();
-  }
-}
-
-void testdrawline()
-{
-  for (int16_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(0, 0, i, display.height() - 1, WHITE);
-    display.display();
-  }
-  for (int16_t i = 0; i < display.height(); i += 4)
-  {
-    display.drawLine(0, 0, display.width() - 1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(0, display.height() - 1, i, 0, WHITE);
-    display.display();
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = display.width() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, WHITE);
-    display.display();
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.height(); i += 4)
-  {
-    display.drawLine(display.width() - 1, 0, 0, i, WHITE);
-    display.display();
-  }
-  for (int16_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, WHITE);
-    display.display();
-  }
-  delay(250);
-}
-
-void testscrolltext(void)
-{
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10, 0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
-
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
 }
